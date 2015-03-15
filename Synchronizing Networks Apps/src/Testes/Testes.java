030ab5dd0bc8533bf31a60bfb5b72d755b087471
Joao Almeida <Joao.rafael.almeida@ua.pt>
2015-03-15 21:39:14 +0000

    Testes com trocas de mensagens em UDP

--- Synchronizing Networks Apps/src/Testes/Testes.java
+++ Synchronizing Networks Apps/src/Testes/Testes.java
@@ -5,17 +5,58 @@
  */
 package Testes;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 /**
  *
  * @author Rafael
  */
 public class Testes {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("hello");
+    private static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws UnknownHostException, SocketException, IOException 
+    {
+        System.out.println("Introduza o endereço ip de destino: ");
+        String ip = in.nextLine();
+        while(true)
+        {
+            System.out.println("Mensagem a enviar: ");
+            String msg = in.nextLine();
+
+            //Sender
+            byte[] buffer = msg.getBytes();
+            InetAddress address = InetAddress.getByName(ip);
+            DatagramPacket packet = new DatagramPacket(
+                    buffer, buffer.length, address, 5000
+                    );
+            DatagramSocket datagramSocket = new DatagramSocket();
+            datagramSocket.send(packet);
+
+            //Receiver
+            try
+            {
+                DatagramSocket serverSocket = new DatagramSocket(5000);
+                byte[] receiveData = new byte[8];
+                String sentence = "";
+                while(sentence.length() >0)
+                {
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    serverSocket.receive(receivePacket);
+                    sentence = new String( receivePacket.getData());
+                    System.out.println(sentence);
+                }
+            }
+            catch (Exception e)
+            {
+                System.err.println(e);
+            }
+        }
     }
     
 }
